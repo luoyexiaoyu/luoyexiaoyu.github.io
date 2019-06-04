419ba370b355bdf373d209095493db70ebaebb45
luoyexiaoyu <1819650049@qq.com>
2019-06-04 17:19:35 +0800

    zhuye

--- js/public.js
+++ js/public.js
@@ -19,3 +19,31 @@ function ado() {
 	} //audio标签在谷歌浏览器不能自动播放 设置一个点击事件
 }
 ado();
+
+var data = [
+	"欢迎大家",
+	"来到我的",
+	"个人主页"
+];
+
+function writer(text,i,callback) {
+	if(i < text.length) {
+		document.getElementById("text").innerHTML = text.substring(0, i + 1)
+		setTimeout(function () {
+            writer(text, i + 1, callback)
+        }, 150);
+	}
+	else if (typeof callback == 'function') {
+        setTimeout(callback, 1000);
+    }
+}
+
+function startAnimation(i) {
+	if (i < data[i].length) {
+		writer(data[i],0,function() {
+			startAnimation(i + 1)
+		})
+	}
+}
+
+startAnimation(0)
